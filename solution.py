8650ed1ff8c89f266fca05b7e6a749d78875fbd2
ttimoff <47722752+ttimoff@users.noreply.github.com>
2023-04-07 14:55:22 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -8,4 +8,5 @@ def solution(x: np.array) -> float:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return x.mean() / 10 # Ваш ответ
+    ans = (x.mean() -29 + np.random.exponential(1)) / 10
+    return ans # Ваш ответ
